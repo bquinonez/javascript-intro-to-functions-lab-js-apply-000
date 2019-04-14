a8ec82410b6d0de671797990bf3b5e471f731054
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-04-14 00:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,3 @@ function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 
-function logWhisper(string){
-  return console.log(string.toLowerCase())
-}
-
-function sayHiToGrandma(string){
-  if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you, Grandma.") return "I love you, too.";
-  else if (string === string.toUpperCase()) return "YES INDEED!";
-}
